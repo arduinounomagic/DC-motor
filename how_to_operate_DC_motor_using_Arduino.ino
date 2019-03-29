c658e2fbfade5d6d03298427e9470c7c9e6a587c
arduinounomagic <arduinounomagic@gmail.com>
2019-03-29 17:06:02 +0530

    Add files via upload

---
+++ how_to_operate_DC_motor_using_Arduino.ino
@@ -0,0 +1,27 @@
+/*
+*How to operate DC motor using Arduino uno
+*
+*This is a very basic example which will help you to understand how a DC motor can be turn on and off using Arduino Uno.
+*
+*for more detail about this project please visit:
+*
+*
+*
+*Copyright (C) 2007 Free Software Foundation, Inc. <arduinounomagic@gmail.com>
+*
+*for more projects based on Arduino uno please visit: https://arduinounomagic.blogspot.com/
+*/
+const int motor=6;
+
+void setup()
+{
+  pinMode(motor, OUTPUT);
+  }
+
+void loop()
+{
+  digitalWrite(motor, HIGH);
+ delay(1000);
+  digitalWrite(motor, LOW);
+  delay(1000);
+}
