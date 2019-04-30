c7fa8059d097da20afc9d01b07f58a3ce1cff4aa
arduinounomagic <arduinounomagic@gmail.com>
2019-04-30 18:58:48 +0530

    Update how_to_operate_DC_motor_using_Arduino.ino

--- how_to_operate_DC_motor_using_Arduino.ino
+++ how_to_operate_DC_motor_using_Arduino.ino
@@ -3,11 +3,11 @@
 *
 *This is a very basic example which will help you to understand how a DC motor can be turn on and off using Arduino Uno.
 *
-*for more detail about this project please visit:https://arduinounomagic.blogspot.com/2019/03/how-to-operate-dc-motor-using-arduino.html#more
+*for more detail about this project please visit:https://arduinounomagic.com/2019/03/how-to-operate-dc-motor-using-arduino.html#more
 *
 *Copyright (C) 2007 Free Software Foundation, Inc. <arduinounomagic@gmail.com>
 *
-*for more projects based on Arduino uno please visit: https://arduinounomagic.blogspot.com/
+*for more projects based on Arduino uno please visit: https://arduinounomagic.com/
 */
 const int motor=6;
 
